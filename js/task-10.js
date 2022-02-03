db5679e9d1f00e4783faaaf5a9feb731f8d96aac
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-02-03 20:12:18 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -15,9 +15,10 @@ function createBoxes(amount) {
   amount = refs.input.value;
   const arrayOfDivStrings = [];
 
-  let counter = 20;
+  let width = 20;
+  let heigth = 20;
   for (let i = 0; i < amount; i += 1) {
-    let div = `<div style='background-color: ${getRandomHexColor()}; width: ${(counter += 10)}px; height: ${(counter += 10)}px;'></div>`;
+    let div = `<div style='background-color: ${getRandomHexColor()}; width: ${(width += 10)}px; height: ${(heigth += 10)}px;'></div>`;
     arrayOfDivStrings.push(div);
   }
   refs.box.insertAdjacentHTML("afterbegin", arrayOfDivStrings.join(""));
@@ -26,3 +27,24 @@ function createBoxes(amount) {
 function destroyBoxes() {
   refs.box.innerHTML = "";
 }
+
+//another option:
+// function createBoxes(amount) {
+//   amount = refs.input.value;
+//   const arrayOfDivRefs = [];
+
+//   let width = 30;
+//   let heigth = 30;
+//   for (let i = 0; i < amount; i += 1) {
+//     let div = document.createElement("div");
+//     div.style.backgroundColor = getRandomHexColor();
+//     div.style.width = `${width}px`;
+//     div.style.heigth = `${heigth}px`;
+//     arrayOfDivRefs.push(div);
+//     width += 1;
+//     heigth += 1;
+
+//   }
+
+//   refs.box.append(...arrayOfDivRefs);
+// }
