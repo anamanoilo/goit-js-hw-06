55f24921471d5e621240b01afa64ea8c4441dd1c
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-02-04 09:46:48 +0200

    update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -3,14 +3,17 @@ formRef.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  const elements = event.currentTarget.elements;
-  if (!elements.email.value || !elements.password.value) {
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
     alert("All the fields must be filled");
     return;
   }
   const loginObj = {
-    [elements[0].name]: elements[0].value,
-    [elements[1].name]: elements[1].value,
+    [email.name]: email.value,
+    [password.name]: password.value,
   };
 
   console.log(loginObj);
